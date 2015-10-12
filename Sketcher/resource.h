bc708814263a30bfd07a9bbcda231641bb7c7e7a
Pt0lemaeus <tuxiaomi@gmail.com>
2015-10-12 21:17:30 +0800

    nothing

---
+++ Sketcher/resource.h
@@ -0,0 +1,34 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 Sketcher.rc 使用
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_MAINFRAME                   128
+#define IDR_SketcherTYPE                130
+#define ID_WINDOW_MANAGER               131
+#define ID_FILE_ELEMENT                 32771
+#define ID_ELEMENT_LINE                 32772
+#define ID_LINE_F                       32773
+#define ID_LINE_S                       32774
+#define ID_ELEMENT                      32775
+#define ID_ELEMENT_S                    32776
+#define ID_ELEMENT_LINE32777            32777
+#define ID_LINE_S32778                  32778
+#define ID_LINE_S32779                  32779
+#define ID_LINE_A                       32780
+#define ID_LINE_D                       32781
+#define ID_ELEMENT_RECTANGLE            32782
+#define ID_ELEMENT_CIRCLE               32783
+#define ID_ELEMENT_CURVE                32784
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        310
+#define _APS_NEXT_COMMAND_VALUE         32785
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
